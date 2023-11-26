b45b2378e16b3545e53c8fd962619f1a7133231b
mikefromru <googlockgood@gmail.com>
2023-11-26 23:32:13 +0300

    Added permision in post command

--- handlers/client.py
+++ handlers/client.py
@@ -9,7 +9,7 @@ client_router = Router()
 
 @client_router.message(Command('help'))
 async def command_help_handler(message: Message) -> None:
-    await message.answer(f"This is help part of help, {hbold(message.from_user.full_name)}!")
+    await message.answer(f"This is help part of help, {hbold(message.from_user.id)}!")
 
 @client_router.message(Command('description'))
 async def command_help_handler(message: Message) -> None:
@@ -21,4 +21,7 @@ async def command_start_handler(message: Message) -> None:
 
 @client_router.message(Command('post'))
 async def command_start_handler(message: Message, bot: Bot) -> None:
-    await bot.send_message(chat_id=os.getenv('CHAT_ID'), text='New news')
+    if int(os.getenv('USER_ID')) == message.from_user.id:
+        await bot.send_message(chat_id=os.getenv('CHAT_ID'), text='New news')
+    else:
+        await message.answer(f'You can not use it, {message.from_user.id}')
