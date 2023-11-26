209cfa7722b71b388c5a51cfc8c2abcfe7dd554a
mikefromru <googlockgood@gmail.com>
2023-11-26 21:32:46 +0300

    Added post handler

--- handlers/client.py
+++ handlers/client.py
@@ -2,15 +2,23 @@ from aiogram import Router
 from aiogram.filters import Command 
 from aiogram.utils.markdown import hbold
 from aiogram.types import Message
+from aiogram import Bot
+import os
 
 client_router = Router()
 
 @client_router.message(Command('help'))
 async def command_help_handler(message: Message) -> None:
-    # Bot instance: `bot.send_message(chat_id=message.chat.id, ...)`
     await message.answer(f"This is help part of help, {hbold(message.from_user.full_name)}!")
+
+@client_router.message(Command('description'))
+async def command_help_handler(message: Message) -> None:
+    await message.answer(f"This is help part of description, {hbold(message.from_user.full_name)}!")
 
 @client_router.message(Command('start'))
 async def command_start_handler(message: Message) -> None:
     await message.answer(f"Hello, {hbold(message.from_user.full_name)}!")
 
+@client_router.message(Command('post'))
+async def command_start_handler(message: Message, bot: Bot) -> None:
+    await bot.send_message(chat_id=os.getenv('CHAT_ID'), text='New news')
