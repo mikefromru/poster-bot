52053a7a69d3b9fce9901059b7520a0206773d14
mikefromru <googlockgood@gmail.com>
2023-12-03 02:00:52 +0300

    modifided env file

--- handlers/client.py
+++ handlers/client.py
@@ -6,7 +6,7 @@ from aiogram.types import Message
 from aiogram import Bot
 import os
 
-from news.my_news import get_news
+from news.my_news import get_news, get_funnypics
 
 client_router = Router()
 
@@ -22,24 +22,62 @@ async def command_help_handler(message: Message) -> None:
 async def command_start_handler(message: Message) -> None:
     await message.answer(f"Hello, {hbold(message.from_user.full_name)}!")
 
+
+# https://v.redd.it/h6y4pu255q3c1
+
 @client_router.message(Command('post_mems'))
-async def command_start_handler(message: Message) -> None:
-    while True:
-        await message.answer('Mems!')
-        await asyncio.sleep(10) 
+async def command_mems_handler(message: Message, bot: Bot) -> None:
+    if int(os.getenv('USER_ID')) == message.from_user.id:
+        while True:
+            post = await get_funnypics()
+            if post.get('type') == 'video':
+                await bot.send_video(
+                    chat_id=os.getenv('HUMOR_CHAT_ID'), 
+                    video=post.get('url'),
+                )
+            else:
+                await bot.send_photo(
+                    chat_id=os.getenv('HUMOR_CHAT_ID'), 
+                    photo=post.get('url'),
+                    caption=f'{hbold(post.get("title"))}',
+                )
+            await asyncio.sleep(600)
+
+
+@client_router.message(Command('post_mems__'))
+async def command_mems_handler(message: Message, bot: Bot) -> None:
+    if int(os.getenv('USER_ID')) == message.from_user.id:
+        # post = get_funnypics()
+        # while len(post) != 0:
+        # chat_id=os.getenv('HUMOR_CHAT_ID'), 
+        await bot.send_video(
+                # chat_id=os.getenv('HUMOR_CHAT_ID'), 
+            message.chat.id,
+            'https://v.redd.it/h6y4pu255q3c1',
+                # photo=post[0].get('url'),
+                # caption=f'{hbold(post[0].get("title"))}',
+        )
+            # post.pop(0)
+            # await asyncio.sleep(3) 
+
+
+    # while True:
+        # await message.answer('Mems!')
+        # await asyncio.sleep(10) 
 
 @client_router.message(Command('post_news'))
-async def command_start_handler(message: Message, bot: Bot) -> None:
+async def command_news_handler(message: Message, bot: Bot) -> None:
     if int(os.getenv('USER_ID')) == message.from_user.id:
         while True:
             post = get_news()
             if post != None:
                 try:
                     await bot.send_photo(
-                        chat_id=os.getenv('CHAT_ID'), 
+                        chat_id=os.getenv('NEWS_CHAT_ID'), 
                         photo=post.get('enclosure'),
                         caption=f'{hbold(post.get("title"))}\n\n{post.get("description")}\n{post.get("category")}',
                     )
+
                 except:
                     await bot.send_message(
                         chat_id=os.getenv('CHAT_ID'), 
--- news/my_news.py
+++ news/my_news.py
@@ -2,9 +2,154 @@ import asyncio
 import requests
 from fake_useragent import UserAgent
 from bs4 import BeautifulSoup as bs
-import os
+import os, sys
+import random
+import praw
+import asyncpraw
 
-lst = []
+def get_funny_animals():
+    ua = UserAgent()
+    header = {'User-Agent':  str(ua.chrome)}
+
+    # url = os.getenv('URL_RSS_4')
+    url = 'https://www.reddit.com/r/FunnyAnimals/'
+    try:
+        s = requests.Session()
+
+        r = s.get(url, headers=header)
+        # r = requests.get(url, headers=header)
+    except Exception as err:
+        print(f'Error fetching the URL {url}')
+        return sys.exit()
+
+    soup = bs(r.content, 'lxml')
+    item = soup.find("h1", class_="_2yYPPW47QxD4lFQTKpfpLQ")
+
+    # item = soup.find('h3', class_='_eYtD2XCVieq6emjKBH3m')
+    # item = soup.find_all('h3')
+    # item = soup.find_all('h3')
+    
+    print(item)
+
+# get_funny_animals()
+
+# current_funny_pic = {'id': 3, 'title': None}
+# lst = []
+
+# funny_post = {}
+funny_post = []
+
+async def get_funnypics():
+ 
+    ua = UserAgent()
+    # reddit = praw.Reddit(
+    reddit = asyncpraw.Reddit(
+        client_id=os.getenv('CLIENT_ID'),
+        client_secret=os.getenv('CLIENT_SECRET'),
+        redirect_uri=os.getenv('REDIRECT_URI'),
+        user_agent=str(ua.chrome),
+
+    )
+
+    # posts = await reddit.subreddit("FunnyAnimals").top(limit=100)
+
+    lst_subreddits = ['WTF', 'hmmm', 'memes', 'aww', 'Cursed_Images', 'Funnypics', 'FunnyPhotoshop']
+    # lst_subreddits = ['WTF', 'hmmm']
+    # lst_subreddits = ['FunnyAnimals', 'shitposting', 'Pikabu', 'perfectlycutscreams', 'funny', 'Funnypics']
+    subreddit = await reddit.subreddit(random.choice(lst_subreddits))
+    # subreddit = await reddit.subreddit("perfectlycutscreams")
+    # subreddit = await reddit.subreddit("aww")
+    # posts = subreddit.top(limit=10)
+    # subs = []
+
+
+
+
+    async for post in subreddit.top(limit=100):
+        # print(post.media)
+        if post.is_video:
+            # video_url = post.media["reddit_video"]["fallback_url"]
+            # audio_url = video_url.split("DASH_")[0] + "audio"
+            # audio_url_alternatvie = video_url.split("DASH_")[0] + "DASH_audio.mp4"
+            # dct = {'type': 'video', 'url': audio_url_alternatvie}
+
+            # dct = {'type': 'video', 'url': post.media.get('reddit_video').get('fallback_url')}
+            # funny_post.append(dct)
+            pass
+
+        elif os.path.splitext(post.url)[1] in ['.jpg', '.png']: 
+            if post.subreddit in ['hmmm', 'aww']:
+                dct = {'type': 'picture', 'title': '', 'url': post.url}
+            else:
+                dct = {'type': 'picture', 'title': post.title, 'url': post.url}
+            funny_post.append(dct)
+    
+    return random.choice(funny_post)
+
+
+
+
+
+
+
+
+
+
+    # print(random.choice(funny_post))
+        # subs.append(posts.title)
+
+        # print(list(posts))
+        # posts = reddit.subreddit("perfectlycutscreams").top(limit=10)
+
+        # var = list(posts)
+        # bar = random.choice(var)
+        # if bar.is_video:
+        #     print('video')
+        #     print(bar.media)
+        #     funny_post['type'] = 'video'
+        #     funny_post['url'] = bar.media.get('reddit_video').get('scrubber_media_url')
+
+        # elif os.path.splitext(bar.url)[1] in ['.jpg', '.png']: 
+        #     funny_post['type'] = 'picture'
+        #     funny_post['url'] = bar.url
+        
+        # return funny_post
+
+
+
+
+
+
+
+        # print(bar.title, bar.)
+    # print(random.choice(var.title))
+    # var = random.choice(posts)
+    # print(var.title)
+    # for post in posts:
+        # print(post.title)
+        # if post.is_video:
+            # print(post.media)
+        # scrubber_media_url
+        # 'fallback_url'
+        # if post.is
+        # if post.is_video:
+        # print(post.id, ' <<< post.id')
+            # print(post.url, post.name, ' <<< post.url')
+
+
+        # if os.path.splitext(post.url)[1] == '.jpg' and post.id != current_funny_pic.get('id'): 
+        # if os.path.splitext(post.url)[1] == '.jpg': 
+            # lst.append(rrent_funny_pic['id'] = post.id)
+            # lst.append({'id': post.id, 'title': post.title, 'url': post.url, 'name': post.name})
+            # current_funny_pic['url'] = post.url
+
+    # print(lst, len(lst)) 
+    # return lst
+
+    # ua = UserAgent()
+    # header = {'User-Agent':  str(ua.chrome)}
+
+
 
 current_post = {}
 
@@ -26,7 +171,7 @@ def get_news() -> dict():
     post = {
             'title': item.title.text.strip(), 
             'description': item.description.text.replace('<br />', '').strip(),
-            'category': item.category.text.strip(),
+            # 'category': item.category.text.strip(),
         }
 
     link_pic = item.enclosure.get('url')
@@ -43,3 +188,8 @@ def get_news() -> dict():
     else:
         print('This post is posted already ...')
         return None
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(get_funnypics())
+
