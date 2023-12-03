716a9f7edb65fcb697178c1ea841ae497cec00b2
mikefromru <googlockgood@gmail.com>
2023-12-03 23:45:24 +0300

    Switched from requests to aiohttp

---
+++ memes/local_tools.py
@@ -0,0 +1,28 @@
+
+lst_subreddits = [
+    'meme',
+    'memes', 
+    'dankmemes',
+    'Animemes',
+    'DirtyMemes',
+    'GymMemes',
+    'AdviceAnimals',
+    'hmm',
+    'hmmm',
+    'funny', 
+    'funnysigns', 
+    'Funnypics', 
+    # 'Cursed_Images',
+    # 'Funnypics', 
+    # 'aww', 
+    # 'WTF', 
+    # 'shitposting', 
+    # 'Cursed_Images',
+    # 'FunnyPhotoshop'
+]
+
+# lst_subreddits = ['WTF', 'hmmm', 'memes', 'aww', 'funny', 'shitposting', 'funnysigns', 'Cursed_Images', 'Funnypics', 'FunnyPhotoshop']
+
+# lst_subreddits = ['WTF', 'hmmm']
+# lst_subreddits = ['FunnyAnimals', 'shitposting', 'Pikabu', 'perfectlycutscreams', 'funny', 'Funnypics']
+
--- memes/memes.py
+++ memes/memes.py
@@ -7,39 +7,14 @@ import random
 import praw
 import asyncpraw
 
-def get_funny_animals():
-    ua = UserAgent()
-    header = {'User-Agent':  str(ua.chrome)}
+from PIL import Image, ImageDraw, ImageFont
 
-    # url = os.getenv('URL_RSS_4')
-    url = 'https://www.reddit.com/r/FunnyAnimals/'
-    try:
-        s = requests.Session()
+# Mine
+from .local_tools import lst_subreddits
 
-        r = s.get(url, headers=header)
-        # r = requests.get(url, headers=header)
-    except Exception as err:
-        print(f'Error fetching the URL {url}')
-        return sys.exit()
-
-    soup = bs(r.content, 'lxml')
-    item = soup.find("h1", class_="_2yYPPW47QxD4lFQTKpfpLQ")
-
-    # item = soup.find('h3', class_='_eYtD2XCVieq6emjKBH3m')
-    # item = soup.find_all('h3')
-    # item = soup.find_all('h3')
-    
-    print(item)
-
-# get_funny_animals()
-
-# current_funny_pic = {'id': 3, 'title': None}
-# lst = []
-
-# funny_post = {}
 funny_post = []
 
-async def get_funnypics():
+async def get_memes():
  
     ua = UserAgent()
     # reddit = praw.Reddit(
@@ -53,9 +28,11 @@ async def get_funnypics():
 
     # posts = await reddit.subreddit("FunnyAnimals").top(limit=100)
 
-    lst_subreddits = ['WTF', 'hmmm', 'memes', 'aww', 'Cursed_Images', 'Funnypics', 'FunnyPhotoshop']
+    # lst_subreddits = ['WTF', 'hmmm', 'memes', 'aww', 'funny', 'shitposting', 'funnysigns', 'Cursed_Images', 'Funnypics', 'FunnyPhotoshop']
+    # lst_subreddits = lst_subreddits
     # lst_subreddits = ['WTF', 'hmmm']
     # lst_subreddits = ['FunnyAnimals', 'shitposting', 'Pikabu', 'perfectlycutscreams', 'funny', 'Funnypics']
+
     subreddit = await reddit.subreddit(random.choice(lst_subreddits))
     # subreddit = await reddit.subreddit("perfectlycutscreams")
     # subreddit = await reddit.subreddit("aww")
@@ -65,7 +42,7 @@ async def get_funnypics():
 
 
 
-    async for post in subreddit.top(limit=100):
+    async for post in subreddit.hot(limit=100):
         # print(post.media)
         if post.is_video:
             # video_url = post.media["reddit_video"]["fallback_url"]
@@ -78,10 +55,11 @@ async def get_funnypics():
             pass
 
         elif os.path.splitext(post.url)[1] in ['.jpg', '.png']: 
-            if post.subreddit in ['hmmm', 'aww']:
-                dct = {'type': 'picture', 'title': '', 'url': post.url}
-            else:
-                dct = {'type': 'picture', 'title': post.title, 'url': post.url}
+            # if post.subreddit in ['hmmm', 'aww', 'funny', 'Cursed_Images', 'funnysigns', 'shitposting']:
+                # dct = {'type': 'picture', 'title': '', 'url': post.url}
+            # else:
+            dct = {'type': 'picture', 'url': post.url}
+            # dct = {'type': 'picture', 'title': post.title, 'url': post.url}
             funny_post.append(dct)
     
     return random.choice(funny_post)
@@ -151,45 +129,71 @@ async def get_funnypics():
 
 
 
-current_post = {}
 
-def get_news() -> dict():
-    global current_post
-    ua = UserAgent()
-    header = {'User-Agent':  str(ua.chrome)}
-    url = os.getenv('URL_RSS_1')
+from PIL import Image
+import pytesseract
 
-    try:
-        r = requests.get(url, headers=header)
-    except Exception as err:
-        print(f'Error fetching the URL {url}')
-        print(err)
+def change_text_on_pic():
+    # Open the image file
+    # image = Image.open('pic.jpg')
+    # # Perform OCR using PyTesseract
+    # text = pytesseract.image_to_string(image)
+    # # Print the extracted text
+    # print(text)
 
-    soup = bs(r.text, "xml")
 
-    item = soup.find('item')
-    post = {
-            'title': item.title.text.strip(), 
-            'description': item.description.text.replace('<br />', '').strip(),
-            # 'category': item.category.text.strip(),
-        }
 
-    link_pic = item.enclosure.get('url')
 
-    # check a picture for extension
-    # not add the pic that is not related to the post
-    if os.path.splitext(link_pic)[1] != '.png':
-        post['enclosure'] = link_pic
 
-    if current_post.get('title') != post.get('title'):
-        print('Posting ...')
-        current_post = post
-        return current_post
-    else:
-        print('This post is posted already ...')
-        return None
+    # Открываем изображение
+    image_path = 'pic.jpg'
+    image = Image.open(image_path)
 
-if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(get_funnypics())
+    # Создаем объект для рисования на изображении
+    draw = ImageDraw.Draw(image)
+
+    # Определяем координаты и размер области, содержащей текст
+    # В этом примере используются произвольные координаты
+    text_position = (0, 0)
+    text_width = 500
+    text_height = 50
+
+    # Заменяем текст на область фона
+    # В данном случае просто закрашиваем область наложением прямоугольника с цветом фона
+    background_color = (255, 255, 255)  # Цвет фона (белый)
+    draw.rectangle([text_position, (text_position[0] + text_width, text_position[1] + text_height)], fill=background_color)
+
+    # Сохраняем обработанное изображение
+    processed_image_path = 'image.jpg'
+    image.save(processed_image_path)
+
+
+
+
+def foo():
+    image = Image.open('pic.jpg')
+    draw = ImageDraw.Draw(image)
+
+    # Выбор шрифта и размера текста
+    font = ImageFont.truetype("OpenSans-Bold.ttf", 36)  # Здесь вы можете выбрать другой шрифт и размер
+
+    # Определение текста, который нужно заменить
+    text_to_replace = "ME BEING PREPARED FOR 2020:"
+
+    # Задание координат и замена текста
+    x, y = 100, 100  # Примерные координаты, где вы хотите разместить новый текст
+    new_text = "New Text"
+    draw.text((x, y), new_text, fill="black", font=font)
+
+    # Сохранение измененного изображения
+    image.save('modified_image.jpg')
+
+
+
+
+# if __name__ == "__main__":
+    # change_text_on_pic()
+    # foo()
+    # loop = asyncio.get_event_loop()
+    # loop.run_until_complete(get_funnypics())
 
