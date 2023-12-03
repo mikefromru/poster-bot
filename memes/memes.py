7e1c061ac5970af0da7f40eac2be5aed37693822
mikefromru <googlockgood@gmail.com>
2023-12-04 02:11:50 +0300

    Added Dockerfile

--- memes/memes.py
+++ memes/memes.py
@@ -4,7 +4,6 @@ import os
 import random
 import asyncpraw
 
-from PIL import Image, ImageDraw, ImageFont
 
 # Mine
 from .local_tools import lst_subreddits
@@ -14,7 +13,6 @@ funny_post = []
 async def get_memes():
  
     ua = UserAgent()
-    # reddit = praw.Reddit(
     reddit = asyncpraw.Reddit(
         client_id=os.getenv('CLIENT_ID'),
         client_secret=os.getenv('CLIENT_SECRET'),
@@ -22,40 +20,15 @@ async def get_memes():
         user_agent=str(ua.chrome),
 
     )
-
-    # posts = await reddit.subreddit("FunnyAnimals").top(limit=100)
-
-    # lst_subreddits = ['WTF', 'hmmm', 'memes', 'aww', 'funny', 'shitposting', 'funnysigns', 'Cursed_Images', 'Funnypics', 'FunnyPhotoshop']
-    # lst_subreddits = lst_subreddits
-    # lst_subreddits = ['WTF', 'hmmm']
-    # lst_subreddits = ['FunnyAnimals', 'shitposting', 'Pikabu', 'perfectlycutscreams', 'funny', 'Funnypics']
-
     subreddit = await reddit.subreddit(random.choice(lst_subreddits))
-    # subreddit = await reddit.subreddit("perfectlycutscreams")
-    # subreddit = await reddit.subreddit("aww")
-    # posts = subreddit.top(limit=10)
-    # subs = []
-
-
-
 
     async for post in subreddit.hot(limit=100):
-        # print(post.media)
         if post.is_video:
-            # video_url = post.media["reddit_video"]["fallback_url"]
-            # audio_url = video_url.split("DASH_")[0] + "audio"
-            # audio_url_alternatvie = video_url.split("DASH_")[0] + "DASH_audio.mp4"
-            # dct = {'type': 'video', 'url': audio_url_alternatvie}
-            # dct = {'type': 'video', 'url': post.media.get('reddit_video').get('fallback_url')}
-            # funny_post.append(dct)
+            # print(post.media)
             pass
 
         elif os.path.splitext(post.url)[1] in ['.jpg', '.png']: 
-            # if post.subreddit in ['hmmm', 'aww', 'funny', 'Cursed_Images', 'funnysigns', 'shitposting']:
-                # dct = {'type': 'picture', 'title': '', 'url': post.url}
-            # else:
             dct = {'type': 'picture', 'url': post.url}
-            # dct = {'type': 'picture', 'title': post.title, 'url': post.url}
             funny_post.append(dct)
     
     return random.choice(funny_post)
